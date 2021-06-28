ce6dca62bc1c83f1e93a828049fb5fdefc004c33
wwhai <751957846@qq.com>
2021-06-28 23:11:33 +0800

    fix(statistic): fix map concurency problem

---
+++ test/publish.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {1..5000}; do
+    mosquitto_pub -h 192.168.0.103 -p 1883 -t '$X_IN_END' -m "{\"temp\": $RANDOM,\"hum\":$RANDOM}"
+    echo "Publish ", $i, "{\"temp\": $RANDOM,\"hum\":$RANDOM} Ok."
+done
