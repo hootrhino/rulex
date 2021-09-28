c4d6ed7f4fe8e241e6c8bb658abe2b91981f2938
wwhai <cnwwhai@gmail.com>
2021-09-28 22:15:26 +0800

    stage: for dev stage

--- test/cloud_api_test.go
+++ test/cloud_api_test.go
@@ -2,7 +2,7 @@ package test
 
 import (
 	"context"
-	"flag"
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -13,26 +13,33 @@ import (
 	cloud "rulex/cloud"
 )
 
-var (
-	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
-)
+// var (
+// 	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
+// )
 
+type AtomicCloudServiceServer struct {
+	cloud.UnsafeAtomicCloudServiceServer
+}
+
+func (s *AtomicCloudServiceServer) CallCloud(ctx context.Context, cs *cloud.Service) (*cloud.CallResult, error) {
+	fmt.Println("----", cs)
+	return nil, nil
+}
 func run() error {
+	server := grpc.NewServer()
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-	err := cloud.RegisterAtomicCloudServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
-	if err != nil {
-		return err
-	}
+	// opts := []grpc.DialOption{grpc.WithInsecure()}
+	// cloud.RegisterAtomicCloudServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	cloud.RegisterAtomicCloudServiceServer(server, &AtomicCloudServiceServer{})
 	return http.ListenAndServe(":8089", mux)
 }
 
 func TestAtomicCloudService(t *testing.T) {
-	flag.Parse()
-	defer glog.Flush()
+	// flag.Parse()
+	// defer glog.Flush()
 
 	if err := run(); err != nil {
 		glog.Fatal(err)
