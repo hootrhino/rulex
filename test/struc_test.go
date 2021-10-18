01ddcc66dfc12e9108c61bf7a115e5d674602f85
wwhai <cnwwhai@gmail.com>
2021-10-18 23:59:06 +0800

    test: compare golang binary and struc

--- test/struc_test.go
+++ test/struc_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"bytes"
+	"encoding/binary"
+	"fmt"
 	"testing"
 
 	"github.com/lunixbochs/struc"
@@ -27,4 +29,21 @@ func Test_Run(t *testing.T) {
 	o := &Example{}
 	err = struc.Unpack(&buf, o)
 	t.Log(err)
+}
+func Test_Run_go_binary(t *testing.T) {
+	inputs := [][]byte{
+		{0x01},
+		{0x02},
+		{0x7f},
+		{0x80, 0x01},
+		{0xff, 0x01},
+		{0x80, 0x02},
+	}
+	for _, b := range inputs {
+		x, n := binary.Uvarint(b)
+		if n != len(b) {
+			fmt.Println("Uvarint did not consume all of in")
+		}
+		fmt.Println(x)
+	}
 }
