5fac9ba66412601827d1194786cb7937953f13f3
wwhai <cnwwhai@gmail.com>
2021-10-21 11:15:26 +0800

    test: add lua to json test

--- test/lua/json_to_table.lua
+++ test/lua/json_to_table.lua
@@ -1,8 +1,6 @@
 local json = require "json"
 
-local t = {}
-
-local encode = json:encode(t)
-print(encode)
-local decode = json:decode(encode)
-print(decode)
+local V1 = json.encode({1, 2, 3, {x = 10}})
+print(V1)
+local V2 = json.decode('[1,2,3,{"x":10}]')
+print(V2[1])
