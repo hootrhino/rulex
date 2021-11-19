090ab19c1025766db7bcbb7dcbeaa98271889c92
wwhai <cnwwhai@gmail.com>
2021-11-19 18:31:46 +0800

    test: add test data to mongodb

---
+++ test/lua/data_to_mongo.lua
@@ -0,0 +1,18 @@
+---@diagnostic disable: undefined-global
+
+-- Success
+function Success()
+    print("======> success")
+end
+-- Failed
+function Failed(error)
+    print("======> failed:", error)
+end
+
+-- Actions
+Actions = {
+    function(data)
+        stdlib:DataToMongo('OUTEND_7ce89673-d3cf-43fe-82c8-0cf4c2be50a8', data)
+        return true, data
+    end
+}
