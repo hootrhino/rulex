9f6294cc3d55d8e44c531471744c340ebb348dab
wwhai <cnwwhai@gmail.com>
2022-01-12 00:30:25 +0800

    fix: fix modbus write error

--- test/lua/write_modbus.lua
+++ test/lua/write_modbus.lua
@@ -14,7 +14,7 @@ Actions = {function(data)
     local e = {}
     e["address"] = 1
     e["quantity"] = 1
-    e["values"] = 1
+    e["value"] = 0xFF00
     t["type"] = 5
     t["params"] = e
     rulexlib:WriteInStream('INEND_85211405-7285-446b-a120-ede14767a89f', rulexlib:JsonEncode(t))
