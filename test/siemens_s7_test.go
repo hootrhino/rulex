60cc757e72c41ac2928042e736eab4cc7a396f46
wwhai <751957846@qq.com>
2022-02-17 10:31:55 +0800

    fix: fix simens S7 test case

--- test/siemens_s7_test.go
+++ test/siemens_s7_test.go
@@ -10,12 +10,9 @@ import (
 )
 
 func Test_server(t *testing.T) {
-
 	server := profinet.NewServer()
-	server.SetOutput([]uint16{11, 22, 33, 44, 55, 66, 77, 88, 99, 100})
-	server.SetInput([]uint16{11, 22, 33, 44, 55, 66, 77, 88, 99, 100})
 	server.SetDB(10, []uint16{11, 22, 33, 44, 55, 66, 77, 88, 99, 100})
-	err := server.Listen("0.0.0.0:1503", 0, 1)
+	err := server.Listen("0.0.0.0:1800", 0, 1)
 	if err != nil {
 		t.Error(err)
 		return
@@ -25,26 +22,20 @@ func Test_server(t *testing.T) {
 }
 
 func client(t *testing.T) {
-	handler := gos7.NewTCPClientHandler("127.0.0.1:1503", 0, 1)
-	err1 := handler.Connect()
+	handler := gos7.NewTCPClientHandler("127.0.0.1:1800", 0, 1)
+
 	defer handler.Close()
-	if err1 != nil {
-		t.Error(err1)
+	if err := handler.Connect(); err != nil {
+		t.Error(err)
 		return
 	}
 	client := gos7.NewClient(handler)
-	// buf := make([]byte, 2)
-	// buf[0] = 24
-	// buf[1] = 34
-	// println("SEND ", binary.BigEndian.Uint16(buf))
-	// if err := client.AGWriteDB(13, 4, 2, buf); err != nil {
-	// 	t.Error(err)
-	// }
-	buf2 := make([]byte, 10)
-	if err := client.AGReadDB(10, 0, 10, buf2); err != nil {
+	dataBuf := make([]byte, 10)
+	if err := client.AGReadDB(10, 0, 10, dataBuf); err != nil {
 		t.Error(err)
+		return
 	}
-	t.Log("client.AGReadDB =>", buf2)
+	t.Log("client.AGReadDB =>", dataBuf)
 
 }
 func Test_gen_config(t *testing.T) {
@@ -67,7 +58,7 @@ func Test_gen_config(t *testing.T) {
 		IdleTimeout  int          `json:"idleTimeout" validate:"required" title:"心跳超时时间" info:""` // 5s
 		Frequency    int64        `json:"frequency" validate:"required" title:"采集频率" info:""`     // 5s
 		StateAddress stateAddress `json:"stateAddress" validate:"required" title:"状态地址" info:""`  // 5s
-		Dbs []db `json:"dbs" validate:"required" title:"采集配置" info:""` // Db
+		Dbs          []db         `json:"dbs" validate:"required" title:"采集配置" info:""`           // Db
 	}
 	c := siemensS7config{
 		Host:        "",
@@ -94,24 +85,18 @@ func Test_gen_config(t *testing.T) {
 *
 *  Error: Address out of range
 *
-*/
+ */
 func Test_readDB(t *testing.T) {
-	handler := gos7.NewTCPClientHandler("10.55.143.60", 0, 1)
+	handler := gos7.NewTCPClientHandler("127.0.0.1:1800", 0, 1)
 
 	defer handler.Close()
-	if err := handler.Connect();err != nil {
+	if err := handler.Connect(); err != nil {
 		t.Error(err)
 		return
 	}
 	client := gos7.NewClient(handler)
-	info,err:= client.GetCPUInfo()
-	if err!=nil{
-		t.Error(err)
-		return
-	}
-	t.Log("CPU:",info.ASName)
-	dataBuf := make([]byte, 20)
-	if err := client.AGReadDB(10, 0, 1, dataBuf); err != nil {
+	dataBuf := make([]byte, 10)
+	if err := client.AGReadDB(10, 0, 10, dataBuf); err != nil {
 		t.Error(err)
 		return
 	}
