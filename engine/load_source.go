26be5da601ec79c3efef4358ab7323758c8b2839
wwhai <cnwwhai@gmail.com>
2022-09-20 21:38:56 +0800

    feat: ready to support ithings

--- engine/load_source.go
+++ engine/load_source.go
@@ -57,6 +57,9 @@ func (e *RuleEngine) LoadInEnd(in *typex.InEnd) error {
 	if in.Type == typex.TENCENT_IOT_HUB {
 		return startSources(source.NewTencentIothubSource(e), in, e)
 	}
+	if in.Type == typex.ITHINGS_IOT_HUB {
+		return startSources(source.NewIThingsSource(e), in, e)
+	}
 
 	return fmt.Errorf("unsupported InEnd type:%s", in.Type)
 }
