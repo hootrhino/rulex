883881224335383b04e77916d4f19379f48d2a88
wwhai <cnwwhai@gmail.com>
2022-10-09 16:22:58 +0800

    dev: remove unused file

--- test/rulexlib_doc_gen_test.go
+++ test/rulexlib_doc_gen_test.go
@@ -4,17 +4,7 @@ import (
 	"testing"
 
 	"github.com/i4de/rulex/device"
-	"github.com/i4de/rulex/engine"
 )
-
-/*
-*
-* 生成标准lua库的文档
-*
- */
-func Test_Gen_rulexlib_doc(t *testing.T) {
-	engine.BuildInLuaLibDoc()
-}
 
 /*
 *
