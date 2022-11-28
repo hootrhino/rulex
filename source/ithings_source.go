58d81c596f6975d7042bc75d2983ae9a7cca0781
wwhai <cnwwhai@gmail.com>
2022-11-28 13:19:25 +0800

    dev: support ithings (#37)
    * dev: support ithings
    
    * fix: fix test error
    
    * test: change log text

--- source/ithings_source.go
+++ source/ithings_source.go
@@ -7,38 +7,29 @@ package source
  */
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"time"
+
 	"github.com/i4de/rulex/common"
 	"github.com/i4de/rulex/core"
 	"github.com/i4de/rulex/glogger"
 	"github.com/i4de/rulex/typex"
 	"github.com/i4de/rulex/utils"
-	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 const (
-	// 属性下发称之为控制指令
-	ithings_METHOD_CONTROL       string = "control"
-	ithings_METHOD_CONTROL_REPLY string = "control_reply"
-	// 属性下发称之为控制指令[这是为了兼容一些其他平台，实际上和上面功能完全一一样]
-	ithings_METHOD_PROPERTY       string = "property"
-	ithings_METHOD_PROPERTY_REPLY string = "property_reply"
-	// 动作请求
-	ithings_METHOD_ACTION       string = "action"
-	ithings_METHOD_ACTION_REPLY string = "action_reply"
-)
-
-const (
 	// 属性
-	_ithings_PropertyTopic      = "$thing/down/property/%v/%v"
-	_ithings_PropertyUpTopic    = "$thing/up/property/%v/%v"
-	_ithings_PropertyReplyTopic = "$thing/property/reply/%v/%v"
+	_ithings_PropertyTopic   = "$thing/down/property/%v/%v"
+	_ithings_PropertyUpTopic = "$thing/up/property/%v/%v"
 	// 动作
-	_ithings_ActionTopic      = "$thing/down/action/%v/%v"
-	_ithings_ActionUpTopic    = "$thing/up/action/%v/%v"
-	_ithings_ActionReplyTopic = "$thing/action/reply/%v/%v"
+	_ithings_ActionTopic   = "$thing/down/action/%v/%v"
+	_ithings_ActionUpTopic = "$thing/up/action/%v/%v"
+	// 事件
+	_ithings_EventUpTopic = "$thing/up/event/%v/%v"
 )
 
 /*
@@ -48,12 +39,16 @@ const (
  */
 type ithingsUpMsg struct {
 	Method      string                 `json:"method"`
-	ClientToken string                 `json:"clientToken,omitempty"` // 腾讯云
-	Id          string                 `json:"id,omitempty"`          // 兼容非腾讯云平台
-	Params      map[string]interface{} `json:"params,omitempty"`      // 腾讯云
-	Data        map[string]interface{} `json:"data,omitempty"`        // 兼容非腾讯云平台
+	ClientToken string                 `json:"clientToken,omitempty"`
+	ActionId    string                 `json:"actionId,omitempty"`
+	EventId     string                 `json:"eventId,omitempty"`
+	Params      map[string]interface{} `json:"params,omitempty"`
+	Data        map[string]interface{} `json:"data,omitempty"`
 	Code        int                    `json:"code"`
 	Status      string                 `json:"status"`
+	// 扩展特性
+	UserDefineTopic string `json:"userDefineTopic,omitempty"` // [特性]扩展属性: 用户自定义Topic
+	SubDeviceId     string `json:"subDeviceId,omitempty"`     // 网关子设备ID
 }
 
 //
@@ -97,7 +92,6 @@ func (tc *ithings) Start(cctx typex.CCTX) error {
 	tc.CancelCTX = cctx.CancelCTX
 
 	PropertyTopic := fmt.Sprintf(_ithings_PropertyTopic, tc.mainConfig.ProductId, tc.mainConfig.DeviceName)
-	// 事件
 	ActionTopic := fmt.Sprintf(_ithings_ActionTopic, tc.mainConfig.ProductId, tc.mainConfig.DeviceName)
 	// 服务接口
 	//
@@ -185,6 +179,30 @@ func (*ithings) Topology() []typex.TopologyPoint {
 //
 
 func (tc *ithings) DownStream(bytes []byte) (int, error) {
+	msg := ithingsUpMsg{}
+	if err := json.Unmarshal(bytes, &msg); err != nil {
+		return 0, err
+	}
+	// 属性上报数据有2类：
+	// 1 网关本身
+	// 2 子设备
+	methods := map[string]string{
+		"report":         _ithings_PropertyUpTopic, // 属性上报
+		"controlReply":   _ithings_PropertyUpTopic, // 属性下发结果上报
+		"getStatusReply": _ithings_PropertyUpTopic, // 获取实时状态上报
+		"eventPost":      _ithings_EventUpTopic,    // 事件上报
+	}
+
+	if methods[msg.Method] != "" {
+		topic := fmt.Sprintf(methods[msg.Method], tc.mainConfig.ProductId, tc.mainConfig.DeviceName)
+		err := tc.client.Publish(topic, 1, false, bytes).Error()
+		if err != nil {
+			glogger.GLogger.Error(err)
+		}
+	} else {
+		glogger.GLogger.Error(errors.New("unsupported method:" + msg.Method))
+	}
+
 	return 0, nil
 }
 
@@ -197,7 +215,11 @@ func (*ithings) UpStream([]byte) (int, error) {
 
 func (tc *ithings) subscribe(topic string) {
 	token := tc.client.Subscribe(topic, 1, func(c mqtt.Client, msg mqtt.Message) {
-		// Ithings的消息不走规则引擎, 它纯粹是一个外部资源
+		// 所有的消息都给丢进规则引擎里面, 交给用户的lua脚本来处理
+		work, err := tc.RuleEngine.WorkInEnd(tc.RuleEngine.GetInEnd(tc.PointId), string(msg.Payload()))
+		if !work {
+			glogger.GLogger.Error(err)
+		}
 	})
 	if token.Error() != nil {
 		glogger.GLogger.Error(token.Error())
