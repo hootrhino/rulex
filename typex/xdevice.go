1f46724d6e25f1cfa846dbaa134d6e8736b448f9
wwhai <cnwwhai@gmail.com>
2022-12-04 15:20:28 +0800

    doc: add doc (#41)

--- typex/xdevice.go
+++ typex/xdevice.go
@@ -25,28 +25,28 @@ type DeviceType string
 
 // 支持的设备类型
 const (
-	TSS200V02      DeviceType = "TSS200V02"
-	RTU485_THER    DeviceType = "RTU485_THER"
-	YK08_RELAY     DeviceType = "YK08_RELAY"
-	S1200PLC       DeviceType = "S1200PLC"
+	TSS200V02      DeviceType = "TSS200V02"      // Multi params Sensor
+	RTU485_THER    DeviceType = "RTU485_THER"    // RS485 Sensor
+	YK08_RELAY     DeviceType = "YK08_RELAY"     // YK8 RS485 Relay
+	S1200PLC       DeviceType = "S1200PLC"       // SIMENS-S71200
 	GENERIC_MODBUS DeviceType = "GENERIC_MODBUS" // 通用Modbus
 	GENERIC_UART   DeviceType = "GENERIC_UART"   // 通用串口
-	GENERIC_SNMP   DeviceType = "GENERIC_SNMP"   // 通用GENERIC_SNMP
-	USER_G776      DeviceType = "USER_G776"      // USER_G776
+	GENERIC_SNMP   DeviceType = "GENERIC_SNMP"   // SNMP 支持
+	USER_G776      DeviceType = "USER_G776"      // 有人 G776 4G模组
 	ICMP_SENDER    DeviceType = "ICMP_SENDER"    // ICMP_SENDER
 )
 
 // 设备元数据
 type Device struct {
-	UUID         string                 `json:"uuid"`
+	UUID         string                 `json:"uuid"`         // UUID
 	Name         string                 `json:"name"`         // 设备名称，例如：灯光开关
 	Type         DeviceType             `json:"type"`         // 类型,一般是设备-型号，比如 ARDUINO-R3
 	ActionScript string                 `json:"actionScript"` // 当收到指令的时候响应脚本
 	Description  string                 `json:"description"`  // 设备描述信息
-	BindRules    map[string]Rule        `json:"-"`
-	State        DeviceState            `json:"state"`  // 状态
-	Config       map[string]interface{} `json:"config"` // 配置
-	Device       XDevice                `json:"-"`
+	BindRules    map[string]Rule        `json:"-"`            // 与之关联的规则
+	State        DeviceState            `json:"state"`        // 状态
+	Config       map[string]interface{} `json:"config"`       // 配置
+	Device       XDevice                `json:"-"`            // 实体设备
 }
 
 func NewDevice(t DeviceType,
@@ -83,17 +83,17 @@ type DCAResult struct {
 
 // 真实工作设备,即具体实现
 type XDevice interface {
-	//  初始化 通常用来获取设备的配置
+	// 初始化 通常用来获取设备的配置
 	Init(devId string, configMap map[string]interface{}) error
 	// 启动, 设备的工作进程
 	Start(CCTX) error
-	// 从设备里面读数据出来
+	// 从设备里面读数据出来, 第一个参数一般作flag用, 也就是常说的指令类型
 	OnRead(cmd int, data []byte) (int, error)
-	// 把数据写入设备
+	// 把数据写入设备, 第一个参数一般作flag用, 也就是常说的指令类型
 	OnWrite(cmd int, data []byte) (int, error)
 	// 设备当前状态
 	Status() DeviceState
-	// 停止设备
+	// 停止设备, 在这里释放资源,一般是先置状态为STOP,然后CancelContext()
 	Stop()
 	// 设备属性，是一系列属性描述
 	Property() []DeviceProperty
@@ -103,6 +103,7 @@ type XDevice interface {
 	SetState(DeviceState)
 	// 驱动接口, 通常用来和硬件交互
 	Driver() XExternalDriver
-	// 外部调用
+	// 外部调用, 该接口是个高级功能, 准备为了设计分布式部署设备的时候用, 但是相当长时间内都不会开启
+	// 默认情况下该接口没有用
 	OnDCACall(UUID string, Command string, Args interface{}) DCAResult
 }
