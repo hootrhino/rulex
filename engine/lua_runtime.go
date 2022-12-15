6fe2876c65bff5218c318f2392ab8bf830279bea
wwhai <cnwwhai@gmail.com>
2022-12-15 12:13:20 +0800

    feat: add float parse lib function

--- engine/lua_runtime.go
+++ engine/lua_runtime.go
@@ -28,6 +28,9 @@ func LoadBuildInLuaLib(e typex.RuleX, r *typex.Rule) {
 	r.AddLib(e, "rulexlib", "BS2B", rulexlib.BitStringToBytes(e))
 	r.AddLib(e, "rulexlib", "HToN", rulexlib.HToN(e))
 	r.AddLib(e, "rulexlib", "HsubToN", rulexlib.HsubToN(e))
+	// 浮点数处理
+	r.AddLib(e, "rulexlib", "Bin2F32", rulexlib.BinToFloat32(e))
+	r.AddLib(e, "rulexlib", "Bin2F64", rulexlib.BinToFloat32(e))
 	// URL处理
 	r.AddLib(e, "rulexlib", "UrlBuild", rulexlib.UrlBuild(e))
 	r.AddLib(e, "rulexlib", "UrlBuildQS", rulexlib.UrlBuildQS(e))
