99c06b844ebdf862632d033e4faec78c363be132
dropliu <13594448678@163.com>
2023-06-10 13:54:56 +0800

    feat: add license manager (#122)
    * feat: add license manager

--- plugin/license_manager/license_manager.go
+++ plugin/license_manager/license_manager.go
@@ -5,3 +5,51 @@ package licensemanager
 * 证书管理器
 *
  */
+
+import (
+	"github.com/hootrhino/rulex/typex"
+	"gopkg.in/ini.v1"
+)
+
+type LicenseManager struct {
+	uuid string
+}
+
+func NewLicenseManager() *LicenseManager {
+	return &LicenseManager{
+		uuid: "LicenseManager",
+	}
+}
+
+func (dm *LicenseManager) Init(config *ini.Section) error {
+	return nil
+}
+
+func (dm *LicenseManager) Start(typex.RuleX) error {
+	return nil
+}
+func (dm *LicenseManager) Stop() error {
+	return nil
+}
+
+func (hh *LicenseManager) PluginMetaInfo() typex.XPluginMetaInfo {
+	return typex.XPluginMetaInfo{
+		UUID:     hh.uuid,
+		Name:     "LicenseManager",
+		Version:  "v0.0.1",
+		Homepage: "https://github.com/dropliu/rulex",
+		HelpLink: "https://github.com/dropliu/rulex",
+		Author:   "dropliu",
+		Email:    "13594448678@163.com",
+		License:  "MIT",
+	}
+}
+
+/*
+ *
+ * 服务调用接口
+ *
+ */
+func (cs *LicenseManager) Service(arg typex.ServiceArg) typex.ServiceResult {
+	return typex.ServiceResult{}
+}
