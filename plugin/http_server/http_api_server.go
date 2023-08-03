ff7eff8c5b957ef5c34b8e9a7a1807be10db5a48
wwhai <cnwwhai@gmail.com>
2023-08-03 15:32:20 +0800

    dev: http api refactor (#156)

--- plugin/http_server/http_api_server.go
+++ plugin/http_server/http_api_server.go
@@ -72,9 +72,8 @@ func (s *HttpApiServer) InitDb(dbPath string) {
 	}
 
 	if err != nil {
-		glogger.GLogger.Error(err)
 		// Sqlite 创建失败应该是致命错误了, 多半是环境出问题，直接给panic了, 不尝试救活
-		panic(err)
+		glogger.GLogger.Fatal(err)
 	}
 	// 注册数据库配置表
 	// 这么写看起来是很难受, 但是这玩意就是go的哲学啊(大道至简？？？)
@@ -88,6 +87,7 @@ func (s *HttpApiServer) InitDb(dbPath string) {
 		&model.MApp{},
 		&model.MAiBase{},
 		&model.MModbusPointPosition{},
+		&model.MVisual{},
 	); err != nil {
 		glogger.GLogger.Fatal(err)
 		os.Exit(1)
