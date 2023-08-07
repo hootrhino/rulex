cf029f588dae90c3263a90b0652760e224e90a10
wwhai <cnwwhai@gmail.com>
2023-08-07 19:04:57 +0800

    dev: design RTSP camera support

--- test/device_generic_camera_stream_test.go
+++ test/device_generic_camera_stream_test.go
@@ -12,12 +12,12 @@ import (
 
 /*
 *
-* 摄像头拉流
+* 本地摄像头拉流
 *
  */
-// go test -timeout 30s -run ^Test_Generic_camera github.com/hootrhino/rulex/test -v -count=1
+// go test -timeout 30s -run ^Test_Generic_Local_camera github.com/hootrhino/rulex/test -v -count=1
 
-func Test_Generic_camera(t *testing.T) {
+func Test_Generic_Local_camera(t *testing.T) {
 	engine := RunTestEngine()
 	engine.Start()
 
@@ -33,6 +33,39 @@ func Test_Generic_camera(t *testing.T) {
 			"inputMode":  "LOCAL",
 			"device":     "video0",
 			"rtspUrl":    "rtsp://127.0.0.1",
+			"outputMode": "JPEG_STREAM",
+			"outputAddr": "0.0.0.0:2599",
+		})
+	ctx, cancelF := typex.NewCCTX()
+	if err := engine.LoadDeviceWithCtx(GENERIC_CAMERA, ctx, cancelF); err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(25 * time.Second)
+	engine.Stop()
+}
+
+/*
+*
+* RTSP 拉流
+*
+ */
+// go test -timeout 30s -run ^Test_Generic_RTSP_camera github.com/hootrhino/rulex/test -v -count=1
+
+func Test_Generic_RTSP_camera(t *testing.T) {
+	engine := RunTestEngine()
+	engine.Start()
+
+	hh := httpserver.NewHttpApiServer()
+	if err := engine.LoadPlugin("plugin.http_server", hh); err != nil {
+		glogger.GLogger.Fatal("Rule load failed:", err)
+		t.Fatal(err)
+	}
+	GENERIC_CAMERA := typex.NewDevice(typex.GENERIC_CAMERA,
+		"GENERIC_CAMERA", "GENERIC_CAMERA", map[string]interface{}{
+			"maxThread":  10,
+			"inputMode":  "RTSP",
+			"device":     "video0",
+			"rtspUrl":    "rtsp://192.168.0.101:554/av0_0",
 			"outputMode": "JPEG_STREAM",
 			"outputAddr": "0.0.0.0:2599",
 		})
