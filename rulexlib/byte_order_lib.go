09c7450a7ab9980e4bf971269df7fdae90384ef5
Mr.Qu <qubeibei1995@163.com>
2023-08-18 14:33:25 +0800

    feat(#173): support hex encoding and decoding

--- rulexlib/byte_order_lib.go
+++ rulexlib/byte_order_lib.go
@@ -6,9 +6,10 @@ package rulexlib
 *
  */
 import (
-	"github.com/hootrhino/rulex/typex"
-
+	"encoding/hex"
+	"fmt"
 	lua "github.com/hootrhino/gopher-lua"
+	"github.com/hootrhino/rulex/typex"
 )
 
 //--------------------------------------------------------------------------------------------------
@@ -22,43 +23,83 @@ import (
  */
 func ABCD(rx typex.RuleX) func(*lua.LState) int {
 	return func(l *lua.LState) int {
-		// hexs := l.ToString(2)
-		return 0
+		data := l.ToString(2)
+		start := l.ToInt(3)
+		end := l.ToInt(4)
+		subStr, err := SubStr(data, start, end)
+		if err != nil {
+			l.Push(lua.LString(""))
+			l.Push(lua.LString(err.Error()))
+			return 2
+		}
+
+		l.Push(lua.LString(ReverseString(subStr)))
+		l.Push(lua.LNil)
+		return 2
 	}
 }
 
-/*
-*
-* 处理DCBA序
-*
- */
+// DCBA 将十六进制串里的N个字节"ABCD"逆序成"DCBA"
 func DCBA(rx typex.RuleX) func(*lua.LState) int {
 	return func(l *lua.LState) int {
-		// hexs := l.ToString(2)
-		return 0
+		data := l.ToString(2)
+		start := l.ToInt(3)
+		end := l.ToInt(4)
+		subStr, err := SubStr(data, start, end)
+		if err != nil {
+			l.Push(lua.LString(""))
+			l.Push(lua.LString(err.Error()))
+			return 2
+		}
+
+		l.Push(lua.LString(ReverseString(subStr)))
+		l.Push(lua.LNil)
+		return 2
 	}
 }
 
-/*
-*
-* 处理DCBA序
-*
- */
+// BADC 将十六进制串里的N个字节"ABCD"逆序成"BADC"
 func BADC(rx typex.RuleX) func(*lua.LState) int {
 	return func(l *lua.LState) int {
-		// hexs := l.ToString(2)
-		return 0
+		data := l.ToString(2)
+		start := l.ToInt(3)
+		end := l.ToInt(4)
+		subStr, err := SubStr(data, start, end)
+		if err != nil {
+			l.Push(lua.LString(""))
+			l.Push(lua.LString(err.Error()))
+			return 2
+		}
+		bytes, _ := hex.DecodeString(ReverseString(subStr))
+		l.Push(lua.LString(hex.EncodeToString(ReverseByteOrder(bytes))))
+		l.Push(lua.LNil)
+		return 2
 	}
 }
 
-/*
-*
-* 处理CDAB序
-*
- */
+// CDAB 将十六进制串里的2个字节"ABCD"转化成"CDAB"
 func CDAB(rx typex.RuleX) func(*lua.LState) int {
 	return func(l *lua.LState) int {
-		// hexs := l.ToString(2)
-		return 0
+		data := l.ToString(2)
+		start := l.ToInt(3)
+		end := l.ToInt(4)
+		subStr, err := SubStr(data, start, end)
+		if err != nil {
+			l.Push(lua.LString(""))
+			l.Push(lua.LString(err.Error()))
+			return 2
+		}
+		bytes, _ := hex.DecodeString(subStr)
+		l.Push(lua.LString(hex.EncodeToString(ReverseByteOrder(bytes))))
+		l.Push(lua.LNil)
+		return 2
 	}
+}
+
+// SubStr 字符串截取子串，遵循前闭后开原则[start:end)
+func SubStr(data string, start, end int) (string, error) {
+	if start < 0 || end < 0 || start > end || end > len(data) {
+		return "", fmt.Errorf("slice bounds out of range [%d:%d] with length %d", start, end, len(data))
+	}
+	return data[start:end], nil
 }
