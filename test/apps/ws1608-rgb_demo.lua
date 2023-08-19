31606e4c6ad91b34ce151725873daf8dce4ec02f
wwhai <cnwwhai@gmail.com>
2023-08-19 22:20:55 +0800

    dev: add s805 rgb demo

--- test/apps/ws1608-rgb_demo.lua
+++ test/apps/ws1608-rgb_demo.lua
@@ -1,10 +1,29 @@
--- 应用名称 "ws1608:GPIO Test"
-
+---@diagnostic disable: undefined-global
+--
+-- App use lua syntax, goto https://hootrhino.github.io for more document
+-- APPID: APP6b28330ff4be4b0ba2f3e9317c4e2a47
+--
+AppNAME = "LED-RGB"
+AppVERSION = "1.0.0"
+AppDESCRIPTION = ""
+--
+-- Main
+--
 function Main(arg)
     while true do
         ws1608:GPIOSet("red", 1)
-        applib:Sleep(2000)
+        applib:Sleep(200)
         ws1608:GPIOSet("red", 0)
-        applib:Sleep(2000)
+        applib:Sleep(200)
+        --
+        ws1608:GPIOSet("green", 1)
+        applib:Sleep(200)
+        ws1608:GPIOSet("green", 0)
+        applib:Sleep(200)
+        --
+        ws1608:GPIOSet("blue", 1)
+        applib:Sleep(200)
+        ws1608:GPIOSet("blue", 0)
+        applib:Sleep(200)
     end
 end
