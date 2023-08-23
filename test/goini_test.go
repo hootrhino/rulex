28f2dd02e20e6f84a26107ee609581d11bf1cf06
wwhai <cnwwhai@gmail.com>
2023-08-23 10:41:46 +0800

    dev: add CatOsRelease func

--- test/goini_test.go
+++ test/goini_test.go
@@ -17,3 +17,30 @@ func Test_map_ini_file(t *testing.T) {
 		t.Fatal(err1)
 	}
 }
+
+// go test -timeout 30s -run ^TestParse_EtcOsRelease github.com/hootrhino/rulex/test -v -count=1
+func TestParse_EtcOsRelease(t *testing.T) {
+	t.Log(CatOsRelease())
+}
+/*
+*
+* Linux: cat /etc/os-release
+*
+*/
+func CatOsRelease() (map[string]string, error) {
+	returnMap := map[string]string{}
+	cfg, err := ini.ShadowLoad("./data/os-release.conf")
+	if err != nil {
+		return nil, err
+	}
+	DefaultSection, err := cfg.GetSection("DEFAULT")
+	if err != nil {
+		return nil, err
+	}
+	for _, Key := range DefaultSection.KeyStrings() {
+		V, _ := DefaultSection.GetKey(Key)
+		returnMap[Key] = V.String()
+	}
+	return returnMap, nil
+
+}
