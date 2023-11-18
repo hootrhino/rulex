de58c8e3e3c0b3a60dc2da580e2c3b84a0aa4813
wwhai <cnwwhai@gmail.com>
2023-11-18 16:49:30 +0800

    dev: optimize LicenseManager

--- plugin/license_manager/license_manager.go
+++ plugin/license_manager/license_manager.go
@@ -23,17 +23,11 @@ type LicenseManager struct {
 	AdminPassword string
 }
 
-//	admin := {
-//		Username: "open-rhino",
-//		Password: "open-hoot",
-//	}
-//
-// // 出厂的第一台设备
-//
-//	device := {
-//		CpuId:  "o:p:e:n:r:h:i:n:o",
-//		EthMac: "o:p:e:n:h:o:o:t",
-//	}
+/*
+*
+* 开源版本默认是给一个授权证书
+*
+ */
 func NewLicenseManager(r typex.RuleX) *LicenseManager {
 	return &LicenseManager{
 		uuid:          "LicenseManager",
@@ -44,37 +38,44 @@ func NewLicenseManager(r typex.RuleX) *LicenseManager {
 	}
 }
 
-// Init 初始化LicenseManager
 func (l *LicenseManager) Init(section *ini.Section) error {
 	license_path, err1 := section.GetKey("license_path")
+	errMsg := "Load License Public Cipher Failed, May be Your License IS Invalid."
 	if err1 != nil {
-		glogger.GLogger.Fatal("Load License Public Cipher Failed, May be Your License IS Invalid.")
+		glogger.GLogger.Fatal()
+		os.Exit(0)
 	}
 	license_key, err2 := section.GetKey("key_path")
 	if err2 != nil {
-		glogger.GLogger.Fatal("Load License Public Cipher Failed, May be Your License IS Invalid.")
+		glogger.GLogger.Fatal(errMsg)
+		os.Exit(0)
 	}
 	lic, err1 := os.ReadFile(license_path.String())
 	if err1 != nil {
-		glogger.GLogger.Fatal("Load License Public Cipher Failed, May be Your License IS Invalid.")
+		glogger.GLogger.Fatal(errMsg)
+		os.Exit(0)
 	}
 	license := string(lic)
 	key, err2 := os.ReadFile(license_key.String())
 	if err2 != nil {
-		glogger.GLogger.Fatal("Load License Public Cipher Failed, May be Your License IS Invalid.")
+		glogger.GLogger.Fatal(errMsg)
+		os.Exit(0)
 	}
 	private := string(key)
 	localMacSum := SumMd5(fmt.Sprintf("%s,%s", l.CpuId, l.EthMac))
 	localAdminSum := SumMd5(fmt.Sprintf("%s,%s", l.AdminUsername, l.AdminPassword))
 	adminSalt, err3 := DecryptAES(private, license)
 	if err3 != nil {
-		glogger.GLogger.Fatal("Load License Public Cipher Failed, May be Your License IS Invalid.")
+		glogger.GLogger.Fatal(errMsg)
+		os.Exit(0)
 	}
 	if localAdminSum != private {
-		glogger.GLogger.Fatal("Load License Public Cipher Failed, May be Your License IS Invalid.")
+		glogger.GLogger.Fatal(errMsg)
+		os.Exit(0)
 	}
 	if adminSalt != localMacSum {
-		glogger.GLogger.Fatal("Load License Failed, May be Your License IS Invalid.")
+		glogger.GLogger.Fatal(errMsg)
+		os.Exit(0)
 	}
 	glogger.GLogger.Info("Load License Success:", license)
 	return nil
