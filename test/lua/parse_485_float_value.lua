bcfd2650bda88fe84e7dc9d0e5f43d34ffc790da
wwhai <cnwwhai@gmail.com>
2023-11-29 14:27:16 +0800

    dev: add modbus parse demo; fix openwrt sctipt

---
+++ test/lua/parse_485_float_value.lua
@@ -0,0 +1,24 @@
+Actions = {
+    function(args)
+        local dataT, err = json:J2T(args)
+        if (err ~= nil) then
+            stdlib:Throw('parse json error:' .. err)
+            return true, args
+        end
+        for key, value in pairs(dataT) do
+            stdlib:Debug(key .. " :: " .. value['value'])
+            local MatchHexS = hex:MatchHex("humi:[0,3];temp:[4,7];pres:[8,11]", value['value'])
+            local ts = time:Time()
+            local Json = json:T2J(
+                {
+                    tag = key,
+                    ts = ts,
+                    hum = math:TFloat(binary:Bin2F32(MatchHexS['humi'])),
+                    temp = math:TFloat(binary:Bin2F32(MatchHexS['temp'])),
+                    pres = math:TFloat(binary:Bin2F32(MatchHexS['pres'])),
+                })
+            stdlib:Debug(Json)
+        end
+        return true, args
+    end
+}
