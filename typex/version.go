098cdcb3acf3ee2c164b251385b0c20b437ea2a6
wwhai <cnwwhai@gmail.com>
2023-12-05 22:35:48 +0800

    fix: fix rule update missing bind list

--- typex/version.go
+++ typex/version.go
@@ -14,10 +14,10 @@ type Version struct {
 var DefaultVersion = Version{
 	Product:        "COMMON",
 	Version:   `v0.6.4`,
-	ReleaseTime: "2023-12-05 20:54:27",
+	ReleaseTime: "2023-12-05 22:28:39",
 }
 var Banner = `
  **  Welcome to RULEX framework world <'_'>
-**   Version: v0.6.4-5c62e76b4f01f33
+**   Version: v0.6.4-197287bd39f45e9
  **  Document: https://hootrhino.github.io
 `
