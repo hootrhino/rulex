a8801de018dc4b0eb74d670ebbcc34710ac46a34
wwhai <cnwwhai@gmail.com>
2024-01-02 23:57:14 +0800

    dev: update siemens

--- test/siemens_s7_test.go
+++ test/siemens_s7_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	profinet "github.com/Kowiste/ProfinetServer"
+	"github.com/hootrhino/rulex/utils"
 	"github.com/robinson/gos7"
 )
 
@@ -82,4 +83,58 @@ func Test_gen_db_json(t *testing.T) {
 	blocks2 := []S1200BlockValue{}
 	json.Unmarshal(bytes, &blocks2)
 	t.Log(blocks2[0].Value)
+}
+
+// go test -timeout 30s -run ^Test_gen_point github.com/hootrhino/rulex/test -v -count=1
+func Test_gen_point(t *testing.T) {
+	type SiemensDataPoint struct {
+		UUID           string `json:"uuid"`
+		DeviceUUID     string `json:"device_uuid"`
+		SiemensAddress string `json:"siemensAddress"` // 西门子的地址字符串
+		Tag            string `json:"tag"`
+		Alias          string `json:"alias"`
+		DataOrder      string `json:"dataOrder"` // 字节序
+		DataType       string `json:"dataType"`
+		Frequency      *int64 `json:"frequency"`
+		Status         int    `json:"status"`        // 运行时数据
+		LastFetchTime  uint64 `json:"lastFetchTime"` // 运行时数据
+		Value          string `json:"value"`         // 运行时数据
+		// 西门子解析后的地址信息
+		AddressType     string `json:"addressType"`     // 寄存器类型: DB I Q
+		DataBlockType   string `json:"dataBlockType"`   // 数据类型: D X
+		DataBlockNumber *int   `json:"dataBlockNumber"` // 数据块号: 100...
+		ElementNumber   *int   `json:"elementNumber"`   // 元素号:1000...
+		BitNumber       *int   `json:"bitNumber"`       // 位号，只针对I、Q
+	}
+	P := SiemensDataPoint{
+		UUID:       utils.MakeUUID("S7"),
+		DeviceUUID: "123",
+		Tag:        "温度",
+		Frequency: func() *int64 {
+			a := int64(100)
+			return &a
+		}(),
+		SiemensAddress: "DB4900.DBD1000",
+		AddressType:    "DB",
+		DataBlockType:  "DB",
+		DataBlockNumber: func() *int {
+			a := int(4900)
+			return &a
+		}(),
+		ElementNumber: func() *int {
+			a := int(1000)
+			return &a
+		}(),
+		BitNumber: func() *int {
+			a := int(0)
+			return &a
+		}(),
+		DataOrder: "ABCD",
+	}
+	if bytes, err := json.MarshalIndent(P, "", "    "); err != nil {
+		panic(err)
+	} else {
+		t.Log(string(bytes))
+	}
+
 }
--- utils/siemens_addr_parse_util.go
+++ utils/siemens_addr_parse_util.go
@@ -17,6 +17,7 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -28,7 +29,7 @@ import (
 使用符号名称来表示变量或输入/输出地址。这种方式更加直观和易于理解，适用于高级编程语言和工程师使用。例如，可以使用变量名"MotorSpeed"或输入名"I1"来表示对应的地址。
 - 基于字节的地址（Byte-based Addressing）：
 使用字节地址和位地址的组合来表示变量或输入/输出地址。字节地址表示内存中的字节偏移，而位地址表示字节中的位偏移。例如，使用地址"DB1.DBX10.3"表示数据块1中偏移为10的字节的第3位。
-- 基于字的地址（Word-based Addressing）：
+- 基于字的地址（SHORT-based Addressing）：
 类似于基于字节的地址，但是将地址表示为字（16位）的偏移。例如，使用地址"DB1.DBD20"表示数据块1中偏移为20的字。
 - 基于地址区域的地址（Address Area-based Addressing）：
 将地址按照不同的区域进行划分，如输入区域（I），输出区域（Q），数据块区域（DB）等。每个区域都有特定的地址范围。例如，使用地址"I10.3"表示输入区域的第10个输入的第3位。
@@ -38,7 +39,8 @@ import (
 // AddressInfo 包含解析后的地址信息
 type AddressInfo struct {
 	AddressType     string // 寄存器类型: DB I Q
-	DataBlockType   string // 数据类型: B D
+	DataBlockType   string // 数据类型: BYTE SHORT INT
+	DataBlockOrder  string // 字节序
 	DataBlockNumber int    // 数据块号
 	ElementNumber   int    // 元素号
 	BitNumber       int    // 位号，只针对I、Q
@@ -54,28 +56,46 @@ func (O AddressInfo) String() string {
 
 /*
 *
+* 解析数据长度(字节)
+*
+ */
+func ParseRequestSize(AddressType string) (int, error) {
+	switch AddressType {
+	case "BYTE":
+		return 1, nil
+	case "SHORT":
+		return 2, nil
+	case "INT", "FLOAT":
+		return 4, nil
+	default:
+		return 0, errors.New("Invalid Block Type")
+	}
+}
+
+/*
+*
 * 解析地址
 *
  */
-func ParseDB(s string) (AddressInfo, error) {
+func ParseSiemensDB(s string) (AddressInfo, error) {
 	AddressInfo := AddressInfo{}
 	if len(s) < 3 {
 		return AddressInfo, fmt.Errorf("invalid Address format:%s", s)
 	}
 	if s[:2] == "I." {
-		return ParseADDR_I(s)
+		return _ParseADDR_I(s)
 	}
 	if s[:2] == "Q." {
-		return ParseADDR_Q(s)
+		return _ParseADDR_Q(s)
 	}
 	if s[:2] == "IB" {
-		return ParseADDR_IB(s)
+		return _ParseADDR_IB(s)
 	}
 	if s[:2] == "QB" {
-		return ParseADDR_QB(s)
+		return _ParseADDR_QB(s)
 	}
 	if s[:2] == "DB" {
-		return __ParseDB_DX(s)
+		return _ParseDB_DX(s)
 	}
 	return AddressInfo, nil
 
@@ -83,7 +103,7 @@ func ParseDB(s string) (AddressInfo, error) {
 
 // 解析DB: DB4900.DBD2108
 // ^DB\d+\.\w+\.\d+$
-func __ParseDB_DX(s string) (AddressInfo, error) {
+func _ParseDB_DX(s string) (AddressInfo, error) {
 	AddressInfo := AddressInfo{}
 	Len := len(s)
 	if Len < 3 {
@@ -106,18 +126,26 @@ func __ParseDB_DX(s string) (AddressInfo, error) {
 		if len(parts[1]) < 4 {
 			return AddressInfo, fmt.Errorf("Invalid ElementNumber Address format:%s", s)
 		}
-		ElementNumber, err2 := strconv.Atoi(parts[1][4:])
+		ElementNumber, err2 := strconv.Atoi(parts[1][3:]) //DBD...
 		if err2 != nil {
 			return AddressInfo, fmt.Errorf("ElementNumber %s Atoi failed:%s", s, err2)
 		}
-		// DBD: 4字节
-		if parts[1][2] == 'D' {
-			AddressInfo.DataBlockType = "WORD"
-		}
-		// DBX: 1字节
-		if parts[1][2] == 'X' {
-			AddressInfo.DataBlockType = "BYTE"
-		}
+		// // DBD: 4字节
+		// if parts[1][2] == 'D' {
+		// 	AddressInfo.DataBlockType = "INT"
+		// }
+		// // DBD: 4字节
+		// if parts[1][2] == 'W' {
+		// 	AddressInfo.DataBlockType = "SHORT"
+		// }
+		// // DBX: 1字节
+		// if parts[1][2] == 'B' {
+		// 	AddressInfo.DataBlockType = "BYTE"
+		// }
+		// // DBX: 1字节
+		// if parts[1][2] == 'X' {
+		// 	AddressInfo.DataBlockType = "BIT"
+		// }
 		AddressInfo.ElementNumber = ElementNumber
 	}
 	return AddressInfo, nil
@@ -128,7 +156,7 @@ func __ParseDB_DX(s string) (AddressInfo, error) {
 // 读取I寄存器：I + 编号，例如 I0.0、I1.1 等。
 // 写入I寄存器：I + 编号，例如 I0.0、I1.1 等。
 
-func ParseADDR_I(s string) (AddressInfo, error) {
+func _ParseADDR_I(s string) (AddressInfo, error) {
 	AddressInfo := AddressInfo{}
 	if len(s) < 2 {
 		return AddressInfo, fmt.Errorf("invalid Address format:%s", s)
@@ -156,11 +184,13 @@ func ParseADDR_I(s string) (AddressInfo, error) {
 }
 
 // IB0
-func ParseADDR_IB(s string) (AddressInfo, error) {
+func _ParseADDR_IB(s string) (AddressInfo, error) {
 	AddressInfo := AddressInfo{}
 	AddressInfo.AddressType = "IB"
-	AddressInfo.DataBlockNumber = 0
-	AddressInfo.BitNumber = 0
+	DataBlockNumber := 0
+	AddressInfo.DataBlockNumber = DataBlockNumber
+	BitNumber := 0
+	AddressInfo.BitNumber = BitNumber
 	return AddressInfo, nil
 }
 
@@ -168,7 +198,7 @@ func ParseADDR_IB(s string) (AddressInfo, error) {
 // Q（输出）寄存器：
 // 读取Q寄存器：Q + 编号，例如 Q0.0、Q1.1 等。
 // 写入Q寄存器：Q + 编号，例如 Q0.0、Q1.1 等。
-func ParseADDR_Q(s string) (AddressInfo, error) {
+func _ParseADDR_Q(s string) (AddressInfo, error) {
 	AddressInfo := AddressInfo{}
 	if len(s) < 2 {
 		return AddressInfo, fmt.Errorf("invalid Address format:%s", s)
@@ -195,10 +225,12 @@ func ParseADDR_Q(s string) (AddressInfo, error) {
 
 	return AddressInfo, nil
 }
-func ParseADDR_QB(s string) (AddressInfo, error) {
+func _ParseADDR_QB(s string) (AddressInfo, error) {
 	AddressInfo := AddressInfo{}
 	AddressInfo.AddressType = "QB"
-	AddressInfo.DataBlockNumber = 0
-	AddressInfo.BitNumber = 0
+	DataBlockNumber := 0
+	AddressInfo.DataBlockNumber = DataBlockNumber
+	BitNumber := 0
+	AddressInfo.BitNumber = BitNumber
 	return AddressInfo, nil
 }
