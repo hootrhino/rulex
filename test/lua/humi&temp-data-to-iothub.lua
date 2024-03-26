83895a8fee86d5d451f7f08efe023e78a24131cc
wwhai <cnwwhai@gmail.com>
2024-03-26 21:40:36 +0800

    dev: optimize log download

--- test/lua/humi&temp-data-to-iothub.lua
+++ test/lua/humi&temp-data-to-iothub.lua
@@ -26,22 +26,22 @@ Actions = {
         local dataT, err = json:J2T(args)
         if (err ~= nil) then
             Throw('parse json error:' .. err)
-            return true, args
+            return false, args
         end
+        local params = {}
         for _, value in pairs(dataT) do
-            local params = {}
             params[value['tag']] = value.value
-            local json = json:T2J({
-                id = "1",
-                method = "thing.event.property.post",
-                params = params
-            })
-            Debug(json)
-            local err = data:ToMqtt('OUTSKGLIQJX', json)
-            if err ~= nil then
-                Throw(err)
-                return true, args
-            end
+        end
+        local json = json:T2J({
+            id = time:TimeMs(),
+            method = "thing.event.property.post",
+            params = params
+        })
+        -- Debug(json)
+        local err = data:ToMqtt('OUTSKGLIQJX', json)
+        if err ~= nil then
+            Throw(err)
+            return false, args
         end
         return true, args
     end
